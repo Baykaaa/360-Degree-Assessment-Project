f406d62d03a404bb5b0b9fd9601b51923204f144
Baykaa <bayrjargal.d26@tomujin.edu.mn>
2023-03-14 20:29:33 +0800

    Merge branch 'main' of https://github.com/Baykaaa/360-Degree-Assessment-Project

--- Backend/hello.js
+++ Backend/hello.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const config{
-  host: '54.169.69.87',
-  user: 'rtd',
-  password: 'Tiny722$',
-  database: 'pe8db'
-}
-const app = express();
-const port = 3000;
-const bodyParser = require("body-parser");
-app.post(bodyParser())
-const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: '54.169.69.87',
   user: 'rtd',
   password: 'Tiny722$',
   database: 'pe8db'
-})
+});
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+app.post(bodyParser())
+const mysql = require('mysql')
+
 app.post('/hi',(req,res)=>{
 
  
